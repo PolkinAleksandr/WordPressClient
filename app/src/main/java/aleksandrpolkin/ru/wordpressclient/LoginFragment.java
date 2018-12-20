02d390d3de1e19e6831f41d08de761131565ed02
metuct <metuct@mail.ru>
2018-12-20 03:46:51 +0300

    add webViewClient

--- app/src/main/java/aleksandrpolkin/ru/wordpressclient/LoginFragment.java
+++ app/src/main/java/aleksandrpolkin/ru/wordpressclient/LoginFragment.java
@@ -29,8 +29,16 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.screen_fragment_login, container, false);
         WebView web = v.findViewById(R.id.web_view_login);
-
+        web.setWebViewClient(new WebViewClient(){
+                                 @Override
+                                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                                     view.loadUrl(url);
+                                     return true;
+                                 }
+                             });
+        web.getSettings().setJavaScriptEnabled(true);
         web.loadUrl("https://public-api.wordpress.com/oauth2/authorize?client_id=64203&redirect_uri=https://aleksandrpolkin.ru.wordpressclient&response_type=token");
+        //web.loadUrl("https://vk.com/");
         return v;
     }
 
